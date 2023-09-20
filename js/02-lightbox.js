05d80bc0cfca1ecc1cd235228d52a58f815d1ba1
evag2582 <elio.g28@gmail.com>
2023-09-20 18:33:10 -0400

    update task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const createItem = (item) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+};
+
+const itemsContainer = document.querySelector('.gallery');
+for (const item of galleryItems) {
+    const newItem = createItem(item);
+    itemsContainer.innerHTML += newItem;
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+
+});
