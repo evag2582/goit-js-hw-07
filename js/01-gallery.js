e71f59bf5458f923919d8ce623eb10d1a2787616
evag2582 <elio.g28@gmail.com>
2023-09-20 17:15:49 -0400

    update task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const createItem = (item) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+};
+
+const itemsContainer = document.querySelector('.gallery');
+for (const item of galleryItems) {
+    const newItem = createItem(item);
+    itemsContainer.innerHTML += newItem;
+}
+
+itemsContainer.addEventListener("click", imageClick);
+
+function imageClick(e) {
+
+    blockStandartAction(e);
+
+        if (e.target.nodeName !== "IMG") {
+            return;
+        }
+        
+        const instance = basicLightbox.create(`
+        <img src="${e.target.dataset.source}" width = "800" height="600">
+        `);
+    instance.show();
+    
+    itemsContainer.addEventListener("keydown", (e) => {
+        if (e.code === "Escape") {
+            instance.close();
+        }
+    });
+
+    function blockStandartAction(e) {
+        e.preventDefault();
+    }
+}
+    
